e1b08245f692bb63159f661c98e8f2f17263168f
DESKTOP-2NDNHK4\allen <allen.suzanne64@gmail.com>
2021-03-25 19:57:12 -0400

    first

---
+++ GlampApp/GlampApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GlampApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GlampApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ GlampApp/GlampApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GlampApp.Models;
+using GlampApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GlampApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
